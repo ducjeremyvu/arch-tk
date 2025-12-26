624d1a38e838698e98022c801a561254a9018fcb
Duc Jeremy Vu <ducjeremyvu@gmail.com>
2025-12-26 15:30:12 +0100

    added some more scripts for waybar, paru apps and more tools for root setup :

--- scripts/11_paru_apps.sh
+++ scripts/11_paru_apps.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 paru -S mbpfan-git
+
+sudo cp mbpfan.service /etc/systemd/system/
+sudo systemctl enable mbpfan.service
+sudo systemctl daemon-reload
+sudo systemctl start mbpfan.service
+
 paru neovim
 paru firefox 
---
+++ scripts/21_waybar.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+pacman -S --noconfirm \
+  gtkmm3 \
+  jsoncpp \
+  libsigc++ \
+  fmt \
+  wayland \
+  chrono-date \
+  spdlog \
+  gtk3 \
+  gobject-introspection \
+  libgirepository \
+  libpulse \
+  libnl \
+  libappindicator-gtk3 \
+  libdbusmenu-gtk3 \
+  libmpdclient \
+  sndio \
+  libevdev \
+  libxkbcommon \
+  upower \
+  meson \
+  cmake \
+  scdoc \
+  wayland-protocols \
+  glib2-devel
